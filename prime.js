345d87b1549e9dc5ae2b930ff1b02700a4f09cf4
riyashri07 <riyampct7@gmail.com>
2022-09-14 15:44:31 +0530

    please review prime code

---
+++ prime.js
@@ -0,0 +1,16 @@
+function prime(num) {
+
+
+  let count = 0;
+
+  for (let i = 1; i <= num; i++) {
+    if (num % i == 0) {
+      count
+    }
+  }
+  if (factor == 2) {
+    return "Prime"
+  } else {
+   return "Not Prime" 
+  }
+}
